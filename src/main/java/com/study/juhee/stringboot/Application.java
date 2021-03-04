e1fafff0d825fb627fb8e68d5d5d1dd4467ded96
juhee <juheeya401@gmail.com>
2021-03-04 20:23:05 +0900

    주석내용 변경

--- src/main/java/com/study/juhee/stringboot/Application.java
+++ src/main/java/com/study/juhee/stringboot/Application.java
@@ -13,8 +13,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        // 굳이 외장 WAS가 아닌 spring내부 WAS를 사용함(.jar파일로 실행 할 거임)
+        // spring내부 WAS(톰캣)를 사용(.jar파일로 실행 할 거임)
         SpringApplication.run(Application.class, args);
-        // '내장 WAS사용을 권장' : 언제 어디서나 같은 환경에서 스프링부트를 배포할 수 있기 때문.
+        // '내장 WAS사용을 권장'하는 이유 = 언제 어디서나 같은 환경에서 스프링부트를 배포할 수 있기 때문이다.
     }
 }
