9cbe9f4023711205c031be2e09b0dc6db8a2bba2
juhee <juheeya401@gmail.com>
2021-03-17 16:53:32 +0900

    [master] @JsonFormat 테스트

--- src/main/java/com/study/juhee/stringboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/study/juhee/stringboot/web/dto/HelloResponseDto.java
@@ -1,7 +1,10 @@
 package com.study.juhee.stringboot.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
 @RequiredArgsConstructor // final 필드의 생성자를 생성해줌.
@@ -10,4 +13,6 @@ public class HelloResponseDto {
     private final String name;
     private final int amount;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private LocalDateTime currentTime;
 }
